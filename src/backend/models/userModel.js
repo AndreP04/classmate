78e6831bc15ccfc82b37a4c7912ea5c74a621d06
Andre Pretorius <andrepret4@icloud.com>
2025-05-21 20:45:25 +0200

    Mongo DB connection and User Model
    Set up the utility to connect to the MongoDB database, as well as set up the user model

---
+++ src/backend/models/userModel.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+
+    firstName: {
+        type: String,
+        require: true
+    },
+    lastName: {
+        type: String,
+        require: true
+    },
+    username: {
+        type: String,
+        require: true
+    },
+    password: {
+        type: String,
+        require: true
+    },
+    role: {
+        type: String,
+        require: true
+    }
+    
+});
+
+const userModel = mongoose.model('userModel', userSchema);
---
+++ src/backend/utils/connectDB.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+/**
+ * Function to connect to the MongoDB database.
+ * @param {*} mongoURI - Connection string of database.
+ */
+const connectDB = async (mongoURI) => {
+
+        mongoose.connect(mongoURI, 
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true
+            })
+            .then(() => console.info("Mongo database connected successfully."))
+            .catch(err => console.error(`Failed to connect to Mongo database: ${error}`));
+}
+
+export { connectDB };
