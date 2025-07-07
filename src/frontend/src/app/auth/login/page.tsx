e00f6136009d26407b258037af4bf51fceae7156
Andre Pretorius <andrepret4@icloud.com>
2025-07-07 20:05:38 +0200

    Clean install of Next.js frontend

--- src/frontend/src/app/auth/login/page.tsx
+++
@@ -1,7 +0,0 @@
-import LoginForm from "@/app/components/LoginForm";
-
-const LogInPage = () => {
-    return <LoginForm />
-};
-
-export default LogInPage;
--- src/frontend/src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Page = () => {
-  return <h1>ClassMate</h1>
-};
-
-export default Page;
