f216ea87caf21f2b571d6f3eb460b9ad3a9e9edc
André Pretorius <andrepret4@icloud.com>
2025-08-01 21:11:23 +0200

    Update table to material TailwindCSS table in Admin Management Portal

--- src/frontend/classmate/src/app/pages/AdminPortalForm.tsx
+++ src/frontend/classmate/src/app/pages/AdminPortalForm.tsx
@@ -1,31 +1,100 @@
 const AdminPortalForm = () => {
     return (
-        <table className="table-auto">
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Surname</th>
-                    <th>Email</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Educator Name</td>
-                    <td>Educator Surname</td>
-                    <td>Educator Email</td>
-                </tr>
-                <tr>
-                    <td>Educator Name</td>
-                    <td>Educator Surname</td>
-                    <td>Educator Email</td>
-                </tr>
-                <tr>
-                    <td>Educator Name</td>
-                    <td>Educator Surname</td>
-                    <td>Educator Email</td>
-                </tr>
-            </tbody>
-        </table>
+        <form className="bg-[#f5f5f6]">
+            <div className="w-full flex justify-between items-center mb-3 mt-1 pl-3">
+                <div>
+                    <h3 className="text-lg font-semibold text-slate-800">Educators</h3>
+                </div>
+                <div className="ml-3">
+                    <div className="w-full max-w-sm min-w-[200px] relative">
+                        <div className="relative">
+                            <input
+                                className="bg-white w-full pr-11 h-10 pl-3 py-2 bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded transition duration-200 ease focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md"
+                                placeholder="Find an educator" />
+                            <button
+                                className="absolute h-8 w-8 right-1 top-1 my-auto px-2 flex items-center bg-white rounded "
+                                type="button"
+                            >
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="3" stroke="currentColor" className="w-8 h-8 text-slate-600">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div><div className="relative flex flex-col w-full h-full overflow-scroll text-gray-700 bg-white shadow-md rounded-lg bg-clip-border">
+                <table className="w-full text-left table-auto min-w-max">
+                    <thead>
+                        <tr>
+                            <th className="p-4 border-b border-slate-200 bg-slate-50">
+                                <p className="text-sm font-semibold leading-none text-slate-500">
+                                    First Name
+                                </p>
+                            </th>
+                            <th className="p-4 border-b border-slate-200 bg-slate-50">
+                                <p className="text-sm font-semibold leading-none text-slate-500">
+                                    Last Name
+                                </p>
+                            </th>
+                            <th className="p-4 border-b border-slate-200 bg-slate-50">
+                                <p className="text-sm font-semibold leading-none text-slate-500">
+                                    Email Address
+                                </p>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr className="hover:bg-slate-50 border-b border-slate-200">
+                            <td className="p-4 py-5">
+                                <p className="block text-sm text-slate-800">Pieter</p>
+                            </td>
+                            <td className="p-4 py-5">
+                                <p className="text-sm text-slate-800">Roux</p>
+                            </td>
+                            <td className="p-4 py-5">
+                                <p className="text-sm text-slate-800">pieterroux@gmail.com</p>
+                            </td>
+                        </tr>
+                        <tr className="hover:bg-slate-50 border-b border-slate-200">
+                            <td className="p-4 py-5">
+                                <p className="block text-sm text-slate-800">Helena</p>
+                            </td>
+                            <td className="p-4 py-5">
+                                <p className="text-sm text-slate-800">Pieterse</p>
+                            </td>
+                            <td className="p-4 py-5">
+                                <p className="text-sm text-slate-800">helenapieterse87@icloud.com</p>
+                            </td>
+
+                        </tr>
+
+                    </tbody>
+                </table>
+
+                <div className="flex justify-between items-center px-4 py-3">
+                    <div className="text-sm text-slate-500">
+                        Showing <b>1-2</b> of 10
+                    </div>
+                    <div className="flex space-x-1">
+                        <button className="px-3 py-1 min-w-9 min-h-9 text-sm font-normal text-slate-500 bg-white border border-slate-200 rounded hover:bg-slate-50 hover:border-slate-400 transition duration-200 ease">
+                            Prev
+                        </button>
+                        <button className="px-3 py-1 min-w-9 min-h-9 text-sm font-normal text-white bg-slate-800 border border-slate-800 rounded hover:bg-slate-600 hover:border-slate-600 transition duration-200 ease">
+                            1
+                        </button>
+                        <button className="px-3 py-1 min-w-9 min-h-9 text-sm font-normal text-slate-500 bg-white border border-slate-200 rounded hover:bg-slate-50 hover:border-slate-400 transition duration-200 ease">
+                            2
+                        </button>
+                        <button className="px-3 py-1 min-w-9 min-h-9 text-sm font-normal text-slate-500 bg-white border border-slate-200 rounded hover:bg-slate-50 hover:border-slate-400 transition duration-200 ease">
+                            3
+                        </button>
+                        <button className="px-3 py-1 min-w-9 min-h-9 text-sm font-normal text-slate-500 bg-white border border-slate-200 rounded hover:bg-slate-50 hover:border-slate-400 transition duration-200 ease">
+                            Next
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </form>
     );
 };
 
