760dbf6a4c22c8f47e4084d97a235c1f1a124bc0
Andre Pretorius <andrepret4@icloud.com>
2025-08-09 12:18:05 +0200

    Added search box in tables to find students/educators

--- src/frontend/classmate/src/app/pages/AdminPortalForm.tsx
+++ src/frontend/classmate/src/app/pages/AdminPortalForm.tsx
@@ -59,6 +59,34 @@ const AdminPortalForm = () => {
         {/* Header and search */}
         <div className="flex flex-col sm:flex-row justify-between items-center mb-8">
           <h3 className="text-gray-700 text-4xl font-extrabold tracking-tight">Educators</h3>
+          <div className="mt-4 sm:mt-0 w-full sm:w-64 relative">
+            <input
+              type="text"
+              placeholder="Find an educator"
+              // value={}
+              // onChange={}
+              className="w-full h-10 pl-4 pr-10 rounded-md border border-slate-600 bg-slate-700 placeholder-slate-400 text-white focus:outline-none focus:ring-2 focus:ring-[#349495] focus:border-[#349495] transition"
+            />
+            <button
+              type="button"
+              className="cursor-pointer absolute top-1.5 right-1.5 h-7 w-7 flex items-center justify-center rounded hover:bg-slate-600 transition"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="2"
+                stroke="currentColor"
+                className="w-5 h-5 text-slate-300"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+                />
+              </svg>
+            </button>
+          </div>
         </div>
 
         {/* Table */}
